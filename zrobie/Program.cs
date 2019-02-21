3ffe6845e9e9c4a51cfc2cf7ddfdf9b31b63a042
Dawid Laskowski <tosziba102pl@gmail.com>
2019-02-21 22:55:42 +0100

    Add project files.

---
+++ zrobie/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zrobie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
